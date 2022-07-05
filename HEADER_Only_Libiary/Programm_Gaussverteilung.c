a48075dea601094428b0cdbcab101bb6798d9a67
cedric <cedricnettler@gmail.com>
2022-07-05 11:24:09 +0200

    astyle Test 1

--- HEADER_Only_Libiary/Programm_Gaussverteilung.c
+++ HEADER_Only_Libiary/Programm_Gaussverteilung.c
@@ -14,97 +14,97 @@
 
 char version[]="V00.1";
 
-void Programm_Start(){
+void Programm_Start() {
 
 }
 
 int main(int argc,char *argv[])
 {
-        char *vvalue = NULL;
-        int option;
-	float expected_value = 1000.0;
-	float std_deviation = 10.0;
-	int amount_of_values = 100;
-	int Programmstart = 0;			//prüft ob: e, s & n eingegeben!
-        opterr = 0;
+    char *vvalue = NULL;
+    int option;
+    float expected_value = 1000.0;
+    float std_deviation = 10.0;
+    int amount_of_values = 100;
+    int Programmstart = 0;			//prüft ob: e, s & n eingegeben!
+    opterr = 0;
 
-	FILE * outstream;
-	outstream = stdout;
+    FILE * outstream;
+    outstream = stdout;
 
-        while ((option = getopt(argc, argv, "e:hn:o:s:v")) != -1)
+    while ((option = getopt(argc, argv, "e:hn:o:s:v")) != -1)
         switch (option)
         {
-		//void print_gaussian_dist ( float expected_value, float std_deviation , int amount_of_values, FILE ∗ output_stream)
-                // -h (help):
-                case 'h':
-                        Header_Hilfekatalog();
-                        break;
-                // -e Liest Erwartungswert ein (float)
-                case 'e':
-                        expected_value = atof(optarg);
-			Programmstart ++;
-                        break;
-		// -s Liest Standartabhweichung ein (float)
-                case 's':
-                        std_deviation = atof(optarg);
-			Programmstart ++;
-                        break;
-		// -n Liest Anzahl der Werte ein (int)
-                case 'n':
-                        amount_of_values = atoi(optarg);
-			Programmstart ++;
-                        break;
-		// -o outstream
-                case 'o':
-                        outstream = fopen(optarg, "w");
-                        break;
-                // -v Version
-                case 'v':
-                        printf("%s \n", version);
-                        break;
-                //optional error handling
-                case '?':
-                        if (optopt == 'e')
-                        {
-                                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
-                        }
-			else if (optopt == 's')
-                        {
-                                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
-                        }
-			else if (optopt == 'n')
-                        {
-                                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
-                        }
-			else if (optopt == 'o')
-                        {
-                                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
-                        }
-                        else if (isprint (optopt))//is character printable
-                        {
-                                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-                        }
-                        else
-                        {
-                                fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-                        }
-                        Header_Hilfekatalog();
-                        return 1;
-		default:
-                abort ();
+        //void print_gaussian_dist ( float expected_value, float std_deviation , int amount_of_values, FILE ∗ output_stream)
+        // -h (help):
+        case 'h':
+            Header_Hilfekatalog();
+            break;
+        // -e Liest Erwartungswert ein (float)
+        case 'e':
+            expected_value = atof(optarg);
+            Programmstart ++;
+            break;
+        // -s Liest Standartabhweichung ein (float)
+        case 's':
+            std_deviation = atof(optarg);
+            Programmstart ++;
+            break;
+        // -n Liest Anzahl der Werte ein (int)
+        case 'n':
+            amount_of_values = atoi(optarg);
+            Programmstart ++;
+            break;
+        // -o outstream
+        case 'o':
+            outstream = fopen(optarg, "w");
+            break;
+        // -v Version
+        case 'v':
+            printf("%s \n", version);
+            break;
+        //optional error handling
+        case '?':
+            if (optopt == 'e')
+            {
+                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
+            }
+            else if (optopt == 's')
+            {
+                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
+            }
+            else if (optopt == 'n')
+            {
+                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
+            }
+            else if (optopt == 'o')
+            {
+                fprintf (stderr, "There are rules! -%c requires an argument! \n", optopt);
+            }
+            else if (isprint (optopt))//is character printable
+            {
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            }
+            else
+            {
+                fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+            }
+            Header_Hilfekatalog();
+            return 1;
+        default:
+            abort ();
         }
 
-	if (Programmstart == 3) {
-		printf("\n\n... starting application ... \n");
-		print_gaussian_dist ( expected_value, std_deviation, amount_of_values, outstream );
-		}
-	else	{
-		printf("\n\nto start algorithm: enter an expected value, a standart deviation and an amount of values:\n");
-		printf("-e [expected value] -s [standart deviation] -n [amount of values]\n\n");
-		printf("enter -h for further information \n");
-		}
+    if (Programmstart == 3) {
+        printf("\n\n... starting application ... \n");
+        print_gaussian_dist ( expected_value, std_deviation, amount_of_values, outstream );
+    }
+    else	{
+        printf("\n\nto start algorithm: enter an expected value, a standart deviation and an amount of values:\n");
+        printf("-e [expected value] -s [standart deviation] -n [amount of values]\n\n");
+        printf("enter -h for further information \n");
+    }
 
-	fclose(outstream);
+    fclose(outstream);
 
-        return 0;
+    return 0;
 }
--- STATIC_Linked_Libiary/main_aufgabe-2.c
+++ STATIC_Linked_Libiary/main_aufgabe-2.c
@@ -16,62 +16,62 @@ char version[]="V00.3";
 
 int main(int argc,char *argv[])
 {
-        char *vvalue = NULL;
-        int option;
-	int start_application = 0;
-        opterr = 0;
+    char *vvalue = NULL;
+    int option;
+    int start_application = 0;
+    opterr = 0;
 
-	FILE * instream;
+    FILE * instream;
 
-        while ((option = getopt(argc, argv, "hsv")) != -1)
+    while ((option = getopt(argc, argv, "hsv")) != -1)
         switch (option)
         {
-                //void print_gaussian_dist ( float expected_value, float std_deviation , int amount_of_values, FILE ∗ output_stream)
-                // -h (help):
-                case 'h':
-                        Header_Hilfekatalog();
-                        break;
-		// -s Starte Programm ...
-		case 's':
-                        start_application = 1;
-                        break;
-		// -v Version
-                case 'v':
-                        printf("%s \n", version);
-                        break;
-                //optional error handling
-                case '?':
-                        if (isprint (optopt))//is character printable
-                        {
-                                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-                        }
-                        else
-                        {
-                                fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
-                        }
-                        Header_Hilfekatalog();
-                        return 1;
-                default:
-                abort ();
+        //void print_gaussian_dist ( float expected_value, float std_deviation , int amount_of_values, FILE ∗ output_stream)
+        // -h (help):
+        case 'h':
+            Header_Hilfekatalog();
+            break;
+        // -s Starte Programm ...
+        case 's':
+            start_application = 1;
+            break;
+        // -v Version
+        case 'v':
+            printf("%s \n", version);
+            break;
+        //optional error handling
+        case '?':
+            if (isprint (optopt))//is character printable
+            {
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            }
+            else
+            {
+                fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+            }
+            Header_Hilfekatalog();
+            return 1;
+        default:
+            abort ();
         }
 
 
-	//Programmaufruf
-	if (start_application == 1) {
-		printf("\n... starting application ...\n");
-		instream = fopen("instream.txt", "r");
-		
-		//error handling: test for file not existing
-        	if (instream == NULL)
-                        {
-                        printf("Error! Datei nicht gefunden\n");
-                        exit(1);
-                    }
-		else
-			static_gaussian_calculation (instream);
-		
-		fclose(instream);
-	}
+    //Programmaufruf
+    if (start_application == 1) {
+        printf("\n... starting application ...\n");
+        instream = fopen("instream.txt", "r");
 
-        return 0;
+        //error handling: test for file not existing
+        if (instream == NULL)
+        {
+            printf("Error! Datei nicht gefunden\n");
+            exit(1);
+        }
+        else
+            static_gaussian_calculation (instream);
+
+        fclose(instream);
+    }
+
+    return 0;
 }
